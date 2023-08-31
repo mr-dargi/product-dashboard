3c39c01b8a80f57d34a537f0dd818e7e7f3e437d
mr-dargi <dargimohammadreza@gmail.com>
2023-08-31 14:17:09 +0330

    Add React bootstrap package and also recive data from Api and show it as products inside our page

---
+++ src/components/Product.js
@@ -0,0 +1,50 @@
+import {useState, useEffect} from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export default function Product() {
+  // Save Api data in this state
+  const [products, setProducts] = useState([]);
+
+  // Api fetch with useEffect
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, []);
+
+  // Create cards component
+  const cards = products.map(product => {
+    return (
+      <div className="col-md-3 text-center" style={{ marginBottom: "10px" }}>
+        <Card key={product.id} className="h-100">
+          <div className="text-center">
+            <Card.Img
+              variant="top" src={product.image}
+              style={{ width: "100px", height: "130px" }}
+            />
+          </div>
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>price: ${product.price}</Card.Text>
+          </Card.Body>
+
+          <Card.Footer style={{ backgroundColor: "white" }}>
+            <Button variant="primary">
+              Add to cart
+            </Button>
+          </Card.Footer>
+        </Card>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Product dashboard</h1>
+      <div className="row">
+        {cards}
+      </div>
+    </div>
+  )
+}
